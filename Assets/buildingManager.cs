0a21c6d06ca710b24b346b8f27c6494cce78c275
lucas-kern <lucasdkern@gmail.com>
2022-06-29 09:39:12 -0500

    proof of concept with shapes

---
+++ Assets/buildingManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class buildingManager : MonoBehaviour
+{
+    public GameObject[] objects;
+    private GameObject pendingObject;
+
+    private Vector3 pos;
+
+    private RaycastHit hit;
+    [SerializeField] private LayerMask layerMask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pendingObject != null)
+        {
+            pendingObject.transform.position = pos;
+            if(Input.GetMouseButtonDown(0))
+            {
+                PlaceObject();
+            }
+        }
+    }
+
+    void PlaceObject()
+    {
+        pendingObject = null;
+    }
+
+    void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            pos = hit.point;
+        }
+    }
+
+    public void SelectObject(int index)
+    {
+        pendingObject = Instantiate(objects[index], pos, transform.rotation);
+    }
+}
