6066abf4c6ab07e4507cf69d65d79d94bd3d6cba
lucas-kern <lucasdkern@gmail.com>
2023-07-27 00:03:44 -0500

    Create basic build manager that can place shapes from a menu on to the map

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingManager : MonoBehaviour
+{
+    public GameObject[] objects;
+    public GameObject pendingObject;
+
+    private Vector3 pos;
+    private RaycastHit hit;
+    [SerializeField] private LayerMask layerMask;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pendingObject != null)
+        {
+            pendingObject.transform.position = pos;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceObject();
+            }
+        }
+    }
+
+    public void PlaceObject()
+    {
+        pendingObject = null;
+    }
+
+    private void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            pos = hit.point;
+        }
+    }
+
+    public void SelectObject(int index)
+    {
+        pendingObject = Instantiate(objects[index], pos, transform.rotation);
+    }
+}
